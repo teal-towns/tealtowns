ee193127c5c53e2404fee6006c0b0d60b97ac087
Luke Madera <luke.madera@gmail.com>
2023-12-12 21:17:43 -0800

    style: home

--- frontend/lib/modules/home.dart
+++ frontend/lib/modules/home.dart
@@ -19,6 +19,7 @@ class _HomeComponentState extends State<HomeComponent> {
   Widget build(BuildContext context) {
     String text = "# TealTowns \n\n" +
       "We believe the **loneliness and climate crises** can be solved by connecting people together to work on local green (nature based) projects. Specifically, to build TealTowns: **connected X-minute cities** where humans and nature thrive.\n\n" +
+      "Our mission, charge and decision making framework is: **green your town with neighbors**. All team members are empowered to make any decision by asking: **Will this connect neighbors to reduce their carbon footprint?** If yes, do / support it. Otherwise, do not.\n\n" +
       "## How? \n\n" +
       "We use technology (**AI Urban Planner and 3D Visualizer**) and **local leaders** to make it easy for people to **meet their neighbors and green their city**, through both public and private (home improvement) projects.\n\n" +
       "We connect people and food hyper locally. We do this by adding in **soil boxes, community gardens, regenerative farms and Superblocks** to existing cities, and we design new cities where these are the default.\n\n" +
@@ -29,9 +30,9 @@ class _HomeComponentState extends State<HomeComponent> {
       "## Money \n\n" +
       "We are a non-profit, donation based organization of mostly local volunteers plus a paid team at \$30k, \$60k, \$90k, \$120k levels, with a goal to reduce cost of living so everyone can live happily on \$60k or less (with a transition phase of working quarter or part time as each person works toward that). It must be obvious to anyone that mission comes above money.\n\n" +
       "## Initiatives \n\n" +
-      "We have 4 initiatives: 1. **Connect** (grow 100+ people local TealTowns teams doing local green projects), 2. **Own** (build a TealTowns local ownership (including housing) network to provide \$1000 or less down payment housing and other co-purchases). 3. **Plan** (build an AI Urban Planner web product), 4. **Visualize** (build an Unreal 3D immersive experience of the plans).\n\n" +
+      "We have 4 initiatives: 1. **Connect** (grow 100+ people local TealTowns teams doing local green projects), 2. **Own** (build a TealTowns local ownership (including housing) network to provide \$1000 or less down payment housing and other co-purchases). 3. **Plan** (build an AI Urban Planner web / mobile product), 4. **Visualize** (build a 3D immersive experience of the plans).\n\n" +
       "## Focus \n\n" +
-      "Our first 2 focuses are to 1. build a **local shared ownership network** in Concord, CA, USA (and other pilot Teal Towns) and 2. build the AI Urban Planner and Visualizer products around initial projects in Concord (**Superblocks and Concord ReUse**). Through that we will also trial out and select our first 6 to 8 paid team members and then raise \$3 million in donations to start paying them.\n\n" +
+      "Our first 2 focuses are to 1. build a **local shared ownership network** in Concord, CA, USA (and other pilot Teal Towns) and 2. build the AI Urban Planner and Visualizer products around initial projects in Concord (**Superblocks and Concord ReUse**). Through that we will also trial and select our first 6 to 8 paid team members and raise \$3 million in donations to pay them.\n\n" +
       "## Get Involved \n\n" +
       "See our blog for more details!\n\n" +
       "Contact us to get involved: luke.madera@gmail.com\n\n";
