96b5e9976029b0012a17fb896bb84762f582b277
layla-tadjpour <layla.tadjpour@gmail.com>
2024-01-18 20:02:22 -0800

    add tree-detection modules and test

--- tests/test_land_vision.py
+++ tests/test_land_vision.py
@@ -1,4 +1,9 @@
-# import cv2
+import cv2
+import numpy as np
+import os
+from land_vision import evaluate 
+from land_vision.models import SFANet as SFANet
+from land_vision import preprocess 
 # import json
 
 # from mapbox import mapbox_polygon as _mapbox_polygon
@@ -19,3 +24,36 @@
 #     print ('ret', json.dumps(ret['tile'], indent = 2))
 
 #     # _land_vision.seeLand(imageUrl)
+
+def test_urban_tree_detection_inference():
+    img_path = "./uploads/images/naip_image.png"
+    log_dir = "./land_vision/pretrained"
+    min_distance = 1
+    threshold_abs = None
+    threshold_rel = 0.2
+    max_distance = 10
+
+    img = cv2.imread(img_path)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
+    images = img_rgb[None,:,:,:]
+    
+    preprocess = eval(f'preprocess.preprocess_RGB')
+    training_model, model = SFANet.build_model(
+        images.shape[1:],
+        preprocess_fn=preprocess)
+
+    weights_path = os.path.join(log_dir,'weights.best.h5')
+    training_model.load_weights(weights_path)
+
+    preds = model.predict(images,verbose=True,batch_size=1)[...,0]
+    results = evaluate.predict(preds=preds,
+        min_distance=min_distance,
+        threshold_rel=threshold_rel,
+        threshold_abs=threshold_abs,
+        max_distance=max_distance)
+
+    assert isinstance(results['pred_locs'][0], np.ndarray)
+    assert results['pred_locs'][0].shape[1] == 2
+
+    
+
